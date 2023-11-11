7e68c76911cd2a866213e29facd65c21a4584a43
Tejaswi434 <141810258+Tejaswi434@users.noreply.github.com>
2023-11-11 23:23:01 +0530

    Update index.js

--- src/components/SuggestionItem/index.js
+++ src/components/SuggestionItem/index.js
@@ -1,17 +1,30 @@
 // Write your code here
 
-const Suggestionitem = props => {
-  const {everyitem} = props
-  const {suggestion} = everyitem
-  return (
-    <li>
-      <p>{suggestion}</p>
+import './index.css'
 
-      <button type="button">
-        <img src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png" />
-      </button>
+const Suggestionitem = props => {
+  const {item, maindispaly} = props
+  const {suggestion} = item
+
+  const display = () => {
+    maindispaly(suggestion)
+  }
+  return (
+    <li className="list">
+      <div className="simple_element">
+        <p>{suggestion}</p>
+
+        <button type="button" alt="text" className="outer_but">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
+            alt="ok"
+            className="but_size"
+            onClick={display}
+          />
+        </button>
+      </div>
     </li>
   )
 }
 
-export default suggestionitem
+export default Suggestionitem
